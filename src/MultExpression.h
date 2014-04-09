dafaf7bef4456362c5c7b98b7cafd1ef522ea839
alexs10 <alexs10@ufl.edu>
2014-04-09 17:35:37 -0400

    Added a multiply function (bugs with 2 multexpressions)

--- src/MultExpression.h
+++ src/MultExpression.h
@@ -16,8 +16,8 @@ using namespace std;
 class MultExpression: public AbstractNumber {
 public:
     MultExpression(const string &input);
-	MultExpression(vector<AbstractNumber*> expression , vector<AbstractNumber*> dem);
-	MultExpression(vector<AbstractNumber*> expression);
+	MultExpression(vector<AbstractNumber*> nums , vector<AbstractNumber*> dem, char sign);
+	MultExpression(vector<AbstractNumber*> nums, char sign);
 	virtual ~MultExpression();
 	AbstractNumber * add(AbstractNumber *number);
 	AbstractNumber * multiply(AbstractNumber *number);
