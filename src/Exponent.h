d7af22c83d86eb05cdfe898ca83163a167a75e90
cphazen <cphazen@ufl.edu>
2014-04-11 17:23:52 -0400

    Exponent update

--- src/Exponent.h
+++ src/Exponent.h
@@ -5,17 +5,17 @@
 class Exponent: public AbstractNumber
 {
     public:
-        Exponent(tr1::shared_ptr<AbstractNumber>, tr1::shared_ptr<AbstractNumber>);
-        Exponent(tr1::shared_ptr<AbstractNumber>, tr1::shared_ptr<AbstractNumber>, char sign);
-        tr1::shared_ptr<AbstractNumber> add(tr1::shared_ptr<AbstractNumber>);
-        tr1::shared_ptr<AbstractNumber> multiply(tr1::shared_ptr<AbstractNumber>);
-        tr1::shared_ptr<AbstractNumber> divide(tr1::shared_ptr<AbstractNumber>);
+        Exponent(tr1::shared_ptr<AbstractNumber>base, tr1::shared_ptr<AbstractNumber>power);
+        Exponent(tr1::shared_ptr<AbstractNumber>base, tr1::shared_ptr<AbstractNumber>power, char sign);
+        tr1::shared_ptr<AbstractNumber> add(tr1::shared_ptr<AbstractNumber>number);
+        tr1::shared_ptr<AbstractNumber> multiply(tr1::shared_ptr<AbstractNumber>number);
+        tr1::shared_ptr<AbstractNumber> divide(tr1::shared_ptr<AbstractNumber>number);
         tr1::shared_ptr<AbstractNumber> simplify();
         string getName();
         string toString();
         double toDouble();
     	char getSign();
-    	char calcSign(tr1::shared_ptr<AbstractNumber>);
+    	char calcSign(tr1::shared_ptr<AbstractNumber>number);
     	char sign;
     	tr1::shared_ptr<AbstractNumber> getValue(string name);
 
